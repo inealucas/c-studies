b67f4136b0728cb4d6e24a248944c5bf8abd9b00
Lucas Sousa <lucas537023@gmail.com>
2026-05-18 15:10:00 -0300

    Add integers, floating point, and comments examples

---
+++ c/comments.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    // Things I like to do :p
+
+    /*
+        Below are some things
+        that I like to do.
+    */
+
+    printf("I really like procrastinating\n");
+    printf("You may find it strange, but it is one of the things I like to do.\n");
+
+    return 0;
+}
---
+++ c/integers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    int year = 2026;
+    int quantity = 3;
+
+    printf("You are %d years old\n", age);
+    printf("The year is %d\n", year);
+    printf("You ordered %d items\n", quantity);
+
+    return 0;
+}
