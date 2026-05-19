ce81c0a0e625037f061942ae6cb16c46852305d8
Lucas Sousa <lucas537023@gmail.com>
2026-05-19 01:14:25 -0300

    Add booleans, characters, and strings examples

---
+++ c/characters.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+int main() {
+
+
+bool isOnline = false;
+
+printf ("%d\n\n\n", isOnline);
+
+
+
+
+
+//Single characters
+char grade = 'F';
+char symbol = '!';
+char currency = '$';
+
+//Strings (arrays of characters)
+char name[] ="Lucas Sousa";
+char food[] = "Sushi";
+char email[] = "fofinho123@gmail.com";
+
+
+printf("Your email is %s\n", email);
+printf("Your favorite food is %s\n", food);
+printf("Hello i am %s\n\n", name);
+
+printf("My grade is %c\n", grade);
+printf("My favorite symbol is %c\n", symbol);
+printf("The currency is %c\n", currency);
+
+
+
+
+
+
+
+return 0;
+}
